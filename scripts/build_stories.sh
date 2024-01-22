806fb7911152312d694d87ca6e7b4be3623899e4
Stephan Kulla <git.mail@kulla.me>
2024-01-22 20:08:40 +0100

    feat: Add index of build stories

--- scripts/build_stories.sh
+++ scripts/build_stories.sh
@@ -4,6 +4,13 @@
 
 OUTPUT_DIRECTORY=public
 STORIES_DIRECTORY=stories
+INDEX_HTML='<!DOCTYPE html>
+<html>
+<head>
+  <title>Directory of stories</title>
+</head>
+<body>
+<ul>'
 
 mkdir -p $OUTPUT_DIRECTORY
 find $OUTPUT_DIRECTORY -type f -exec rm {} +
@@ -13,4 +20,14 @@ for STORY_FILENAME in `ls "$STORIES_DIRECTORY"`; do
 
   scripts/run_tweego.sh -o "$OUTPUT_DIRECTORY/$STORY_ID.html" \
     "$STORIES_DIRECTORY/$STORY_FILENAME"
+
+  INDEX_HTML="$INDEX_HTML
+    <li><a href='./$STORY_ID.html'>$STORY_ID</a></li>"
 done
+
+INDEX_HTML="$INDEX_HTML
+</ul>
+</body>
+</html>"
+
+echo "$INDEX_HTML" > "$OUTPUT_DIRECTORY/index.html"
